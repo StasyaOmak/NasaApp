8e765999383f690f7e6b77a673c4d84ba4eb3534
Anastasiya <ios.developer.by@gmail.com>
2023-11-30 17:05:42 +0200

    Add Notification for the User

--- NasaApplication/AppDelegate/SceneDelegate.swift
+++ NasaApplication/AppDelegate/SceneDelegate.swift
@@ -6,32 +6,65 @@
 //
 
 import UIKit
+import UserNotifications
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    
+    let notificationCenter = UNUserNotificationCenter.current()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    
-            
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-            window = UIWindow(windowScene: windowScene)
-            window?.rootViewController = MainTabBarController()
-
-            
-            window?.makeKeyAndVisible()
-        }
-
         
-            // Навигация
-//            let firstNavController = UINavigationController(rootViewController: HomeViewController())
-//            let secondNavController = UINavigationController(rootViewController: PhotoViewController())
-//            let thirdNavController = UINavigationController(rootViewController: AsteroidViewController())
-//            let fourthNavController = UINavigationController(rootViewController: BookmarksViewController())
-  
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = MainTabBarController()
+        
+        
+        window?.makeKeyAndVisible()
+        
+        requestPermissiontoNotify()
+        scheduleNotification()
     }
-
+    
+    func requestPermissiontoNotify() {
+        
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                print("Permission received")
+            } else {
+                print("permission not received")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = "Discover the Cosmos Daily with NASA"
+        content.body = "Explore the cosmos with today's NASA pic! 📸✨ Open the app now and enjoy your daily dose of space wonders. 🚀🔭"
+        
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 12
+        dateComponents.minute = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: "Identifier", content: content, trigger: trigger)
+        
+        notificationCenter.add(request) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else {
+                print("Notification added")
+            }
+        }
+        
+        let fourthNavController = UINavigationController(rootViewController: BookmarksViewController())
+        
+    }
     
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
@@ -61,6 +94,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
     
-    
+}
 
 
