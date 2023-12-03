8efd7bf742122943513fbca693041ee05a81c7d8
Anastasiya <ios.developer.by@gmail.com>
2023-12-03 17:59:37 +0200

    Change the display of TabBar Images

--- NasaApplication/Controller/MainTabBarController.swift
+++ NasaApplication/Controller/MainTabBarController.swift
@@ -11,26 +11,27 @@ class MainTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         generateTabBar()
         setTabBarAppearance()
     }
     
     private func generateTabBar() {
         viewControllers = [
-            generateVC(viewController: HomeViewController(), title: "Home", image: UIImage(systemName: "house")),
-            generateVC(viewController: PhotoViewController(), title: "Photo", image: UIImage(systemName: "photo")),
-            generateVC(viewController: AsteroidViewController(), title: "Asteroid", image: UIImage(systemName: "bolt")),
-            generateVC(viewController: BookmarksViewController(), title: "Bookmark", image: UIImage(systemName: "bookmark")),
+            generateVC(viewController: HomeViewController(), title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill")),
+            generateVC(viewController: PhotoViewController(), title: "Photo", image: UIImage(systemName: "photo"), selectedImage: UIImage(systemName: "photo.fill")),
+            generateVC(viewController: AsteroidViewController(), title: "Asteroid", image: UIImage(systemName: "bolt"), selectedImage: UIImage(systemName: "bolt.fill")),
+            generateVC(viewController: BookmarksViewController(), title: "Bookmark", image: UIImage(systemName: "bookmark"), selectedImage: UIImage(systemName: "bookmark.fill")),
         ]
     }
     
-    private func generateVC(viewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+    private func generateVC(viewController: UIViewController, title: String, image: UIImage?, selectedImage: UIImage?) -> UIViewController {
         
         let navigationController = UINavigationController(rootViewController: viewController)
         
         navigationController.tabBarItem.title = title
         navigationController.tabBarItem.image = image
+        navigationController.tabBarItem.selectedImage = selectedImage
         navigationController.tabBarItem.titlePositionAdjustment = .init(horizontal: -3, vertical: 0)
         
         return navigationController
@@ -43,4 +44,5 @@ class MainTabBarController: UITabBarController {
         tabBar.unselectedItemTintColor = UIColor(red: 0.00, green: 0.24, blue: 0.57, alpha: 1.00)
         tabBar.backgroundColor = .systemBackground
     }
+    
 }
